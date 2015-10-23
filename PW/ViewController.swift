0d6c117b50c593d79d55d2aad4a3d42eb8d626b8
Hugh Hopkins <hugh.hopkins@gmail.com>
2015-10-23 20:54:17 +0100

    add assets and basic ui

---
+++ PW/ViewController.swift
@@ -0,0 +1,28 @@
+//
+//  ViewController.swift
+//  PW
+//
+//  Created by Hugh Hopkins on 23/10/2015.
+//  Copyright © 2015 io.pwapp. All rights reserved.
+//
+
+import Cocoa
+import CryptoSwift
+
+class ViewController: NSViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+
+    override var representedObject: AnyObject? {
+        didSet {
+        // Update the view, if already loaded.
+        }
+    }
+
+
+}
+
